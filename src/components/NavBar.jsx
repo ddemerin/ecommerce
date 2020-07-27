42120c2ab33ad2bf1e8c93e66e8f6f9c5d4cc6ec
Daniel Demerin <ddemerin.code@gmail.com>
2020-07-27 15:27:29 -0400

    created styled-component within NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 import logo from "../logo.svg";
 
 class NavBar extends Component {
@@ -20,13 +21,36 @@ class NavBar extends Component {
           </li>
         </ul>
         <Link to="/cart" className="ml-auto">
-          <button>
-            <i className="fas fa-cart-plus"></i>
-          </button>
+          <ButtonContainer>
+            <span className="mr-2">
+              <i className="fas fa-cart-plus"></i>
+            </span>
+            my cart
+          </ButtonContainer>
         </Link>
       </nav>
     );
   }
 }
+
+const ButtonContainer = styled.button`
+  padding: 0.2rem 0.5rem;
+  margin: 0.2rem 0.5rem;
+  text-transform: capitalize;
+  font-size: 1.4rem;
+  background: transparent;
+  border: 0.05rem solid var(--lightBlue);
+  border-radius: 0.5rem;
+  color: var(--lightBlue);
+  cursor: pointer;
+  transition: all 0.5s ease-in-out;
+  &:hover {
+    background: var(--lightBlue);
+    color: var(--mainBlue);
+  }
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default NavBar;
