ba2eac8b7dd9b0b848ba60f6868a25438fb0b7fc
Daniel Demerin <ddemerin.code@gmail.com>
2020-08-04 11:43:12 -0400

    ButtonContainer ternary for Add To Cart created

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,4 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
 
 export const ButtonContainer = styled.button`
   padding: 0.2rem 0.5rem;
@@ -8,12 +7,15 @@ export const ButtonContainer = styled.button`
   font-size: 1.4rem;
   background: transparent;
   border: 0.05rem solid var(--lightBlue);
+  border-color: ${(props) =>
+    props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
   border-radius: 0.5rem;
-  color: var(--lightBlue);
+  color: ${(props) => (props.cart ? "var(--mainYellow)" : "var(--lightBlue)")};
   cursor: pointer;
   transition: all 0.5s ease-in-out;
   &:hover {
-    background: var(--lightBlue);
+    background: ${(props) =>
+      props.cart ? "var(--mainYellow)" : "var(--lightBlue)"};
     color: var(--mainBlue);
   }
   &:focus {
