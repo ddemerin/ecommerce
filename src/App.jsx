bf3087535f9be4047942089c16227c135404226a
Daniel Demerin <ddemerin.code@gmail.com>
2020-08-06 11:54:50 -0400

    setup imports for Modal.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Cart from "./components/Cart";
 import Default from "./components/Default";
+import Modal from "./components/Modal";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -19,6 +20,7 @@ class App extends Component {
           <Route path="/cart" component={Cart} />
           <Route component={Default} />
         </Switch>
+        <Modal />
       </>
     );
   }
